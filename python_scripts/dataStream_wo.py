27c3749a9958d60012f8d79d32bd01c3c251b4ee
vektorious <alexander.kutschera@gmail.com>
2017-12-14 10:50:35 +0100

    corrections

--- python_scripts/dataStream_wo.py
+++ python_scripts/dataStream_wo.py
@@ -7,46 +7,46 @@ from plotly.graph_objs import Scatter, Layout, Figure
 import time
 from math import log10
 
-username = 'openplantscience'
-api_key = 'NKnew0HFk61TBOdLZGfg'
-stream_token1 = '0gjylwkcfn'
-stream_token2 = '92dhemrhv0'
+#username = ''
+#api_key = ''
+#stream_token1 = ''
+#stream_token2 = ''
 
-py.sign_in(username, api_key)
+#py.sign_in(username, api_key)
 
-trace1 = Scatter(
-    x=[],
-    y=[],
-    stream=dict(
-        token=stream_token1,
-        maxpoints=200
-    ),
-    name = 'Cell 1'
-)
-trace2 = Scatter(
-    x=[],
-    y=[],
-    stream=dict(
-        token=stream_token2,
-        maxpoints=200
-    ),
-    name = 'Cell 2'
-)
+#trace1 = Scatter(
+#    x=[],
+#    y=[],
+#    stream=dict(
+#        token=stream_token1,
+#        maxpoints=200
+#    ),
+#    name = 'Cell 1'
+#)
+#trace2 = Scatter(
+#    x=[],
+#    y=[],
+#    stream=dict(
+#        token=stream_token2,
+#        maxpoints=200
+#    ),
+#    name = 'Cell 2'
+#)
 
 
-layout = Layout(
-    title='Raspberry Pi Streaming OD'
-)
+#layout = Layout(
+#    title='Raspberry Pi Streaming OD'
+#)
 
-fig = Figure(data=[trace1, trace2], layout=layout)
+#fig = Figure(data=[trace1, trace2], layout=layout)
 
-print py.plot(fig, filename='Raspberry Pi Streaming OD')
+#print py.plot(fig, filename='Raspberry Pi Streaming OD')
 
 index = 0
-stream1 = py.Stream(stream_token1)
-stream2 = py.Stream(stream_token2)
-stream1.open()
-stream2.open()
+#stream1 = py.Stream(stream_token1)
+#stream2 = py.Stream(stream_token2)
+#stream1.open()
+#stream2.open()
 
 s = serial.Serial('/dev/ttyACM0', 9600)
 s.isOpen()
@@ -65,10 +65,10 @@ try:
 		values = [float(i) for i in values]
 		ref_dark, c1_dark, c2_dark, ref_light, c1_light, c2_light = values
 		OD_c1 = -log10((c1_light - c1_dark)/(ref_light - ref_dark))
-		OD_c2 = -log10((c2_light - c2_dark)/(ref_light - ref_dark)) 
+		OD_c2 = -log10((c2_light - c2_dark)/(ref_light - ref_dark))
 		#file.write(timestamp + "," + response + "," + str(OD_c1) + "," + str(OD_c2))
 		print(OD_c1, OD_c2)
-		
+
 		#stream1.write({'x': timestamp, 'y': OD_c1}, reconnect_on=(200,'',408))
 		#time.sleep(1)
                 #stream2.write({'x': timestamp, 'y': OD_c2}, reconnect_on=(200,'',408))
