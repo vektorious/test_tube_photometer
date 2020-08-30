66205ba65ddf3c0dc32204317badaf2aa7ad0971
Alex <alexander.kutschera@gmail.com>
2020-08-30 19:32:07 +0200

    updated script (more comments!)

--- software/arduino/single/tt_photometer_single/src/main.cpp
+++ software/arduino/single/tt_photometer_single/src/main.cpp
@@ -1,84 +1,96 @@
-//Version 1.1 of the test tube photometer software
-//It ignores possible disturbing light by neglecting the "dark values"
+// Version 1.3 of the test tube photometer software
+// It ignores possible disturbing light by neglecting the "dark values"
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 
-// Set the LCD address to 0x27 for a 16 chars and 2 line display
-LiquidCrystal_I2C lcd(0x3F, 16, 2); // adjust first parameter to the I2C address of your LCD. Check it via the I2C scanner
+// Set the LCD address to 0x3F for a 16 chars and 2 line display
+// adjust first parameter to the I2C address of your LCD. Check it via the I2C scanner
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
-int sensor = A1;  // output pin of OPT101 attached to Analog 5
+// Input/Output
+int sensor = A1;  // input pin of OPT101 attached to Analog 1
+int buttonBlank = 9; // input pin for blank button
+int buttonMeasure = 10; // input pin for measure button
+int LED = 8; // output pin for blank button
+
+// Variables
 float opt_signal;
 float OD = 0;
-int LED = 8;
-int buttonBlank = 9;
-int buttonMeasure = 10;
-
 int blank = 0;
 float blankValue = 0;
-bool trouble = false;
-bool blanked = false;
 int measure = 0;
 
-void measureBlank(int multiplier) {
+// Add factor from calibration curve to calculate concentration
+int factor = 0;
 
+bool trouble = false;
+bool blanked = false;
+
+// Function for "Blank" measurements
+void measureBlank(int multiplier) { // multiplier determines how many measurements are pooled
   blankValue = 0;
   
-  for (int dummy = 0; dummy < multiplier; dummy++)
-  {
+  for (int dummy = 0; dummy < multiplier; dummy++){
     delay(100);
     blankValue = (blankValue + analogRead(sensor));
     delay(100);
   }
+  
+  blankValue = blankValue/multiplier;
   blanked = true;
 }
 
-void measureOD(int multiplier) {
-
+// Function for OD measurements
+void measureOD(int multiplier) { // multiplier determines how many measurements are pooled
   opt_signal = 0;
 
-  for (int dummy = 0; dummy < multiplier; dummy++)
-  {
+  for (int dummy = 0; dummy < multiplier; dummy++){
     delay(100);
     opt_signal = (opt_signal + analogRead(sensor));
     delay(100);
   }
 
-  if(blanked == true){
+  if (blanked == true){ // output of OD if blanked
+    opt_signal = opt_signal/multiplier;
     OD = -log10(opt_signal/blankValue);
+    if(factor =! 0){
+      OD = OD*factor;
+    }
   }
 
-  else{
+  else{ // output of the raw value if not blanked
     OD = opt_signal/multiplier;
   }
 }
 
+// Start loop
 void setup() {
 
   delay(100);
-  lcd.begin();
 
   // Turn on the blacklight and print a message.
+  lcd.begin();
   lcd.backlight();
   lcd.print("   Test  Tube    ");
   lcd.setCursor(0, 1);
-  lcd.print("Photometer  v1.2");
+  lcd.print("Photometer  v1.3");
+
   pinMode (LED, OUTPUT); //for LED
   pinMode(buttonBlank, INPUT_PULLUP);
   pinMode(buttonMeasure, INPUT_PULLUP);
   delay(1000);
   lcd.clear();
 
-
-  for (int i = 0; i < 10; i++)
-  {
+  // If any button is pressed during the startup, troubleshooting mode is activated.
+  // (LED is always on & raw value output) 
+  for (int i = 0; i < 10; i++){
     lcd.setCursor(0, 0);
     lcd.print("Starting ... ");
     lcd.print(i);
     measure = digitalRead(buttonMeasure);
     blank = digitalRead(buttonBlank);
     delay(500);
-    if (measure == LOW || blank == LOW)
-    {
+    if (measure == LOW || blank == LOW){
       trouble = true;
       lcd.clear();
       lcd.print("Troubleshooting");
@@ -87,7 +99,6 @@ void setup() {
       delay(1000);
       break;
     }
-    
   }
   
   lcd.clear();
@@ -96,12 +107,13 @@ void setup() {
   lcd.print("to start :)");
 }
 
+// Main loop
 void loop() {
   measure = digitalRead(buttonMeasure);
   blank = digitalRead(buttonBlank);
 
-  if (trouble == true)
-  {
+  // troubleshooting mode
+  if (trouble == true){
     lcd.clear();
     lcd.print("Showing raw signal ...");
     lcd.setCursor(0, 1);
@@ -111,6 +123,7 @@ void loop() {
     delay(500);
   }
 
+  // measuring blank
   else if(blank == LOW){
     lcd.clear();
     lcd.print("Measuring Blank ...");
@@ -126,6 +139,7 @@ void loop() {
     lcd.print("Blanked");
   }
   
+  // measuring sample
   else if(measure == LOW){
     lcd.clear();
     lcd.print("Measuring OD ...");
@@ -135,16 +149,20 @@ void loop() {
     delay(500);
     measureOD(10);
     digitalWrite(LED, LOW);
+
+    
+
     lcd.setCursor(0, 1);
     lcd.print("                ");
     lcd.setCursor(0, 1);
     lcd.print(OD);
-    if(blanked == false){
-      lcd.print("raw value");
+
+    if (factor =! 0){
+      lcd.print(" g/L");
+    }
+
+    else if(blanked == false){
+      lcd.print(" raw value");
     }
   }
-  
 }
-
-
-
